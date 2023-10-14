22878bef4bd78dacb5a928473f17dc132d783058
cldvvv <cldvvv@gmail.com>
2023-10-14 08:28:59 +0300

    abc

--- final_win.py
+++ final_win.py
@@ -6,31 +6,59 @@ from PyQt5.QtWidgets import (
         QGroupBox, QRadioButton,
         QPushButton, QLabel, QListWidget, QLineEdit)
         
-#from instr import *
+from instr import *
 
 class FinalWin(QWidget):
     def __init__(self, exp):
         ''' the window in which a survey is being conducted '''
         super().__init__()
 
-        '''getting data about the experiment'''
+        #getting data about the experiment
         self.exp = exp
 
-        ''' creating and configuring graphic elements:'''
-        #self.ini...
+        # creating and configuring graphic elements:
+        self.initUI()
 
-        '''establishes connections between elements'''
-        #self.conn...
+        #establishes connections between elements
+        # self.connects()
 
-        '''sets the window appearance (label, size, location)'''
-        #self.set_ap...
+        #sets the window appearance (label, size, location)
+        self.set_appear()
         
         # start:
         self.show()
 
     def results(self):
-        self.index = 0
-        return 'blank'
+        if self.exp.person.age < 7:
+            self.index = 0
+            return "there is no data for this age"
+        self.index = (4 * (int(self.exp.test1) + int(self.exp.test2) + int(self.exp.test3)) - 200) / 10
+
+
+        '''if varsta este intre 7 si 8 ani (inclusive)'''
+        if self.exp.person.age == 7 or self.exp.person.age == 8:
+            if self.index >= 21:
+                return txt_res1
+            elif self.index < 21 and self.index >= 17:
+                return txt_res2
+            elif self.index < 17 and self.index >= 12:
+                return txt_res3
+            elif self.index < 12 and self.index >= 6.5:
+                return txt_res4
+            else:
+                return txt_res5
+
+        '''if varsta este intre 9 si 10 ani (inclusive)
+         - scrieti ca in exemplul 7-8 ani de mai sus ajustand indecsii dupa tabelul din prezentare'''
+       
+        '''if varsta este intre 11 si 12 ani (inclusive)
+        - scrieti ca in exemplul 7-8 ani ajustand indecsii dupa tabelul din prezentare'''
+        
+        '''if varsta este intre 13 si 14 ani (inclusive)
+        - scrieti ca in exemplul 7-8 ani ajustand indecsii dupa tabelul din prezentare'''
+        
+        '''if varsta este mai mare egal ca 15 ani
+        - scrieti ca in exemplul 7-8 ani ajustand indecsii dupa tabelul din prezentare'''
 
     def initUI(self):
         ''' creates graphic elements '''
@@ -44,8 +72,7 @@ class FinalWin(QWidget):
 
     ''' sets what the window will look like (label, size, location) '''
     def set_appear(self):
-        '''functia care seteaza aparenta ferestrei (titlu, dimensiune, pozitia pe ecran)'''
-        '''in fisierul instr.py gasiti numele variabilelor in care sunt tinute titlul, latimea, lungimea si pozitia ferestrei'''
-        #self.setWindowTitle(TITLU_FEREASTRA)
-        #self.resize(LATIME_FEREASTRA, LUNGIME_FEREASTRA)
-        #self.move(POZTIA_X, POZITIA_Y)
+        self.setWindowTitle(txt_finalwin)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
--- second_win.py
+++ second_win.py
@@ -6,8 +6,8 @@ from PyQt5.QtWidgets import (
         QGroupBox, QRadioButton,
         QPushButton, QLabel, QListWidget, QLineEdit)
 
-#from instr import *
-#from final_win import *
+from instr import *
+from final_win import *
 
 class Person():
     def __init__(self, name, age):
@@ -37,19 +37,26 @@ class TestWin(QWidget):
         
         # start:
         self.show()
+    
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
 
     def connects(self):
         self.btn_next.clicked.connect(self.next_click)
 
+    ''' sets what the window will look like (label, size, location) '''
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
     def initUI(self):
         ''' creates graphic elements '''
         #self.questionnary = AllQuestions()
         self.btn_next = QPushButton(txt_sendresults, self)
-        '''modificati cele 3 linii comentate de mai jos ca sa instantiati 3 butoane.
-           Cautati variabilele care contin textele in fisierul instr.py'''
-        #self.btn_test1 = butonul 'Start the first test'
-        #self.btn_test2 = butonul 'Start doing squats'
-        #self.btn_test3 = butonul 'Start the final test'
+        self.btn_test1 = QPushButton(txt_starttest1, self)
+        self.btn_test2 = QPushButton(txt_starttest2, self)
+        self.btn_test3 = QPushButton(txt_starttest3, self)
 
 
         self.text_name = QLabel(txt_name)
@@ -70,67 +77,70 @@ class TestWin(QWidget):
         self.line_age.setValidator(self.validator) # age should be a number!
         self.line_age.setValidator(QIntValidator(7, 150))
 
-        '''la fel ca line_age de mai sus, instantiati alte 3 casute de text
-           line_test1 (txt_hinttest1), 
-           line_test2 (txt_hinttest2), 
-           line_test3 (txt_hinttest3)'''
-        
-        self.l_line = QVBoxLayout() #PANOUL DIN STANGA (pt texte, casute de text, butoane)
-        self.r_line = QVBoxLayout() #PANOUL DIN DREAPTA (pt numarator genoflexiuni si temporizator)
-        self.h_line = QHBoxLayout() #PANOUL PRINCIPAL CARE LE CONTINE PE CELE DOUA PANOURI DE MAI SUS
+        self.line_test1 = QLineEdit(txt_hinttest1)
+        self.line_test1.setValidator(self.validator)
+        self.line_test1.setValidator(QIntValidator(0, 150))
+
+        self.line_test2 = QLineEdit(txt_hinttest2)
+        self.line_test2.setValidator(self.validator)
+        self.line_test2.setValidator(QIntValidator(0, 150))
+
+        self.line_test3 = QLineEdit(txt_hinttest3)
+        self.line_test3.setValidator(self.validator)
+        self.line_test3.setValidator(QIntValidator(0, 150))
+    
+        self.l_line = QVBoxLayout()
+        self.r_line = QVBoxLayout()
+        self.h_line = QHBoxLayout()
         self.r_line.addWidget(self.text_timer, alignment = Qt.AlignCenter)
         self.l_line.addWidget(self.text_name, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.line_name, alignment = Qt.AlignLeft) 
         self.l_line.addWidget(self.text_age, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.line_age, alignment = Qt.AlignLeft)
         self.l_line.addWidget(self.text_test1, alignment = Qt.AlignLeft)
-        #self.l_line.addWidget(self.btn_test1, alignment = Qt.AlignLeft)
-        #self.l_line.addWidget(self.line_test1, alignment = Qt.AlignLeft) 
+        self.l_line.addWidget(self.btn_test1, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.line_test1, alignment = Qt.AlignLeft) 
         self.l_line.addWidget(self.text_test2, alignment = Qt.AlignLeft)
-        #self.l_line.addWidget(self.btn_test2, alignment = Qt.AlignLeft) 
+        self.l_line.addWidget(self.btn_test2, alignment = Qt.AlignLeft) 
         self.l_line.addWidget(self.text_test3, alignment = Qt.AlignLeft)
-        #self.l_line.addWidget(self.btn_test3, alignment = Qt.AlignLeft)
-        #self.l_line.addWidget(self.line_test2, alignment = Qt.AlignLeft)
-        #self.l_line.addWidget(self.line_test3, alignment = Qt.AlignLeft) 
+        self.l_line.addWidget(self.btn_test3, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.line_test2, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.line_test3, alignment = Qt.AlignLeft) 
         self.l_line.addWidget(self.btn_next, alignment = Qt.AlignCenter) 
         self.h_line.addLayout(self.l_line)  
         self.h_line.addLayout(self.r_line)        
         self.setLayout(self.h_line)
     
     def next_click(self):
-        self.hide() #se ascunde fereastra curenta pt a face loc celei noi
+        self.hide()
         self.prs = Person(self.line_name.text, int(self.line_age.text()))
         self.exp = Experiment(self.prs, self.line_test1.text(), self.line_test2.text(), self.line_test2.text())
         self.fw = FinalWin(self.exp)
 
     def timer_test1(self):
-        '''timer primul test'''
+        ''' '''
 
     def timer1Event(self):
-        pass
+        ''' '''
 
     def timer2Event(self):
-        pass
+        ''' '''
 
     def timer_bob(self):
-        '''timer genoflexiuni'''
+        ''' '''
 
     def timer3Event(self):
-        pass
+        ''' '''
 
     def timer_final(self):
-        '''temporizatorul final'''
+        ''' '''
 
     def connects(self):
-        self.btn_next.clicked.connect(self.next_click)
-        '''conectarea celorlalte 3 butoane cu functiile asociate
-           respectiv timer_test1, timer_bob, timer_final
-        '''
+        '''conectati butoanele btn_next,   btn_test1,   btn_test2, btn_test3
+        la functiile asociate  next_click, timer_test1, timer_bob, timer_final'''
 
     ''' sets what the window will look like (label, size, location) '''
     def set_appear(self):
-        '''functia care seteaza aparenta ferestrei (titlu, dimensiune, pozitia pe ecran)'''
-        '''in fisierul instr.py gasiti numele variabilelor in care sunt tinute titlul, latimea, lungimea si pozitia ferestrei'''
-        #self.setWindowTitle(TITLU_FEREASTRA)
-        #self.resize(LATIME_FEREASTRA, LUNGIME_FEREASTRA)
-        #self.move(POZTIA_X, POZITIA_Y)
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
